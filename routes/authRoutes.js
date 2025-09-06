776926bc5f42a96251bcb334e94e677d3570570c
Nikhil <nikhilkumar3403@gmail.com>
2025-09-06 16:05:02 +0530

    Fix Some Imports

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -21,17 +21,15 @@ import express from 'express';
 import { isAuthenticated, login, logout, register, resetPassword, sendRestOtp, sendVerifyOtp, verifyEmail } from '../controllers/AuthController.js';
 import userAuth from '../middleware/userAuth.js';
 
+const authRouter = express.Router();
 
-const AuthRouter = express.Router()
+authRouter.post('/register', register);
+authRouter.post('/login', login);
+authRouter.post('/logout', logout);
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
+authRouter.post('/verify-account', userAuth, verifyEmail);
+authRouter.get('/is-auth', userAuth, isAuthenticated);
+authRouter.post('/send-reset-otp', sendRestOtp);
+authRouter.post('/reset-password', resetPassword);
 
-AuthRouter.post('/register', register)
-AuthRouter.post('/login', login)
-AuthRouter.post('/logout', logout)
-AuthRouter.post('/send-verify-otp', userAuth, sendVerifyOtp)
-AuthRouter.post('/verify-account', userAuth, verifyEmail)
-AuthRouter.get('/is-auth', userAuth, isAuthenticated)
-AuthRouter.post('/send-reset-otp', sendRestOtp)
-AuthRouter.post('/reset-password', resetPassword)
-
-
-export default AuthRouter;
+export default authRouter;
