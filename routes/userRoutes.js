ecc3d3d0a78715476ba1a47ec2d83e0a16df714f
Nikhil <nikhilkumar3403@gmail.com>
2025-09-06 16:08:11 +0530

    Fix Some Imports

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,9 +10,8 @@
 import express from 'express';
 import userAuth from '../middleware/userAuth.js';
 import { getUserData } from '../controllers/userController.js';
-const UserRouter = express.Router()
-UserRouter.get('/data', userAuth, getUserData)
-
-export default UserRouter;
+const userRouter = express.Router();
+userRouter.get('/data', userAuth, getUserData);
+export default userRouter;
 
 
